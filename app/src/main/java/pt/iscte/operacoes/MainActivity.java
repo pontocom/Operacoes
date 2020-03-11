4d7732ebbaaa41b650b0102f0226599b1051caf4
Carlos Serrao <carlos.j.serrao@gmail.com>
2020-03-11 13:21:07 +0000

    Small changes on the way it is presented the result.

--- app/src/main/java/pt/iscte/operacoes/MainActivity.java
+++ app/src/main/java/pt/iscte/operacoes/MainActivity.java
@@ -6,9 +6,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    protected EditText valueA, valueB, result;
+    protected EditText valueA, valueB;
+    protected TextView result;
     protected RadioButton add, subtract, multiply, divide;
 
     @Override
@@ -18,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
 
         valueA = (EditText) findViewById(R.id.et_ValueA);
         valueB = (EditText) findViewById(R.id.et_ValueB);
-        result = (EditText) findViewById(R.id.et_Result);
+        result = (TextView) findViewById(R.id.tvResultado);
 
         add = (RadioButton) findViewById(R.id.rbAdd);
         add.setChecked(true);
@@ -28,12 +30,17 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void performOperation(View v) {
-        int va = Integer.parseInt(valueA.getText().toString());
-        int vb = Integer.parseInt(valueB.getText().toString());
+        int va = 0, vb = 0;
+        String s_va = valueA.getText().toString();
+        String s_vb = valueB.getText().toString();
+        if(!s_va.isEmpty() || !s_vb.isEmpty()){
+            va = Integer.parseInt(s_va);
+            vb = Integer.parseInt(s_vb);
+        }
 
-        if(add.isChecked()) result.setText("" + (va + vb));
-        if(subtract.isChecked()) result.setText("" + (va - vb));
-        if(multiply.isChecked()) result.setText("" + (va * vb));
-        if(divide.isChecked()) result.setText("" + (va / vb));
+        if(add.isChecked()) result.setText("The result of " + va + " + " + vb + " is " + (va + vb));
+        if(subtract.isChecked()) result.setText("The result of " + va + " - " + vb + " is " + (va - vb));
+        if(multiply.isChecked()) result.setText("The result of " + va + " x " + vb + " is " +  (va * vb));
+        if(divide.isChecked()) result.setText("The result of " + va + " / " + vb + " is " +  (va / vb));
     }
 }
